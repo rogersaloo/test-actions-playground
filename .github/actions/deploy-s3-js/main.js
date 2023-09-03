811c07c2e79c1dbbb6dc2b4cc0dfdef0a0b1516d
owitrogers@gmail.com <owitrogers@gmail.com>
2023-09-04 08:00:55 +0900

    correct website path

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -11,8 +11,8 @@ function run(){
     // 2) use the inputs to target s3 bucket- upload files
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-
-    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.aamazonaws.com`;
+    http://custom-action-js.s3-website-ap-northeast-1.amazonaws.com
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
     core.setOutput('website-url', websiteUrl)
     // core.notice("Hellllo this is my code");
 }
