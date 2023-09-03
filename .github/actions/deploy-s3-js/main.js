8da0cbe1395898956bcfd6ed1702a916b303e7c0
owitrogers@gmail.com <owitrogers@gmail.com>
2023-09-04 07:41:24 +0900

    custom deploy with js s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -9,8 +9,6 @@ function run(){
     const distFolder = core.getInput('dist-folder', { required: true});
 
     // 2) use the inputs to target s3 bucket- upload files
-    AWS_ACCESS_KEY_ID=AKIAZM6S3XPFX37NSM64
-    
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 syc ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     core.notice("Hellllo this is my code");
