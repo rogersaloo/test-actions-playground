bdee38f7e81bbfe4152da6b145226405fbb1a0a5
owitrogers@gmail.com <owitrogers@gmail.com>
2023-09-04 07:37:58 +0900

    custom deploy with js s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,8 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
-    core.notice("Hellllo this is my code")
+    //  1) get some input values
+    const bucket = core.getInput('bucket', { required: true});
+    const bucketRegion = core.getInput('bucket-region', { required: false});
+    const distFolder = core.getInput('dist-folder', { required: true});
+
+    // 2) use the inputs to target s3 bucket- upload files
+    AWS_ACCESS_KEY_ID=AKIAZM6S3XPFX37NSM64
+    
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 syc ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    core.notice("Hellllo this is my code");
 }
 run();
